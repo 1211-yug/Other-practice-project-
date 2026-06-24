964986343194b00235e73e5c30b16e7bc0c543d1
yug patel <pyug31196@gmail.com>
2026-06-24 22:21:44 +0530

    Javascript ( 23-6-2026 & 24-6-2026 )

---
+++ Javascript_Other_Practice/23-6-2026/script.js
@@ -0,0 +1,40 @@
+// fullName = "Yug Patel";
+// age = 19;
+// price = 100.10;
+// console.log(fullName, age, price);
+
+
+// student is object.
+
+// const student = {
+//     fullName: "Yug Patel",
+//     age: 19,
+//     cgpa: 8.2,
+//     isPass: true,
+// };
+
+// student["age"] = student["age"] + 1;
+
+// console.log(student["age"]);
+
+/* Product object */
+
+// const product = {
+//     title: "pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 10,
+// };
+
+// console.log(product);
+
+/* profile  */
+
+const profile = {
+    name: "@yug_1211",
+    isFollow: false,
+    followers: 123,
+    following: 123,
+};
+
+console.log(profile);
---
+++ Javascript_Other_Practice/24-6-2026/script.js
@@ -0,0 +1,86 @@
+// This is a Single line comment
+
+/* This is a multi-line
+comment */
+
+// Operators
+
+// let a = 10;
+// let b = 2;
+
+// console.log("a = ", a, "b = ", b);
+
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+
+// Unary Operators
+// let a = 10;
+// let b = 2;
+
+// console.log("a = ", a, "b = ", b);
+// a--;
+// console.log("a-- =", a);
+
+// Assignment Operators
+// let a = 10;
+// let b = 12;
+
+// console.log("a = ", a, "b = ", b);
+
+// a += 4;
+// console.log("a += 4 =", a);
+
+// b -= 4;
+// console.log("b -= 4 =", b);
+
+// a *= 5;
+// console.log("a *= 5 =", a);
+
+// b %= 2;
+// console.log("b %= 2 =", b);
+
+// a /= 2;
+// console.log("a /= 2 =", a);
+
+// b **= 4;
+// console.log("b **= 4 =", b);
+
+// Comparison Operators
+// let a = 10;
+// let b = 1;
+
+// console.log("10 == 10", a == b);
+
+// console.log("10 != 30", a != b);
+
+// console.log("10 === 10", a === b);
+
+// console.log("10 !== 10", a !== b);
+
+// console.log("a > b", a > b);
+
+// Logical Operators
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a == 6;
+
+// console.log("a && b", cond1 && cond2);
+
+
+// Conditional (Ternary) Operator
+
+let age = 12;
+
+if (age >= 18) {
+    console.log("Adult");
+}
+
+if (age < 18) {
+    console.log("Minor");
+}
