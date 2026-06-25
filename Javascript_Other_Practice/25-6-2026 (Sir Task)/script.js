468a0796e038091c1f2efdb50e127b14fd08550f
yug patel <pyug31196@gmail.com>
2026-06-25 12:21:36 +0530

    25-6-2026 ( Sir Task for javascript )

---
+++ Javascript_Other_Practice/25-6-2026 (Sir Task)/script.js
@@ -0,0 +1,48 @@
+// Que 1 Print Hello World
+console.log("Hello World");
+
+// Que 2 Add Two Numbers
+// let a = 10;
+// let b = 20;
+// let sum = a + b;
+
+// console.log("a =", a, "b =", b);
+// console.log("sum =", sum);
+
+
+// Que 3 Find the Square Root
+// let a = 25;
+// console.log("25 square Root is :- ", a ** (1 / 2));
+
+// Que 4 Calculate the Area of a Triangle
+// let t = 2;
+// let r = 6;
+// let l = 10;
+// let s = (t + r + l) / 2;
+
+// console.log("Area of Triangle is :- ", s ** (1 / 2));
+
+// Que 5 Swap Two Variables
+// let a = 10;
+// let b = 20;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log("Swap a variables :- ", a);
+// console.log("Swap b variables :- ", b);
+
+// Que 6 Solve Quadratic Equation
+let a = 1;
+let b = -3;
+let c = 2;
+
+let d = (b * b) - (4 * a * c);
+
+let root1 = (-b + Math.sqrt(d)) / (2 * a);
+let root2 = (-b - Math.sqrt(d)) / (2 * a);
+
+console.log("Root 1 =", root1);
+console.log("Root 2 =", root2);
+
