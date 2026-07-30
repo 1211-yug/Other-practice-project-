22343b54dd17f1e0b693fe0a74eb0e9238d9dfed
yug patel <pyug31196@gmail.com>
2026-07-30 20:27:51 +0530

    Update script.js

--- Javascript_Other_Practice/Digital_Clock/script.js
+++ Javascript_Other_Practice/Digital_Clock/script.js
@@ -45,7 +45,7 @@ const days = [
 ];
 
 const months = [
-    "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 ];
 
 let today = new Date();
