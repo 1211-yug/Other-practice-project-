1ac4469befe2fe1a59ccec13514caafcedf7be9f
yug patel <pyug31196@gmail.com>
2026-08-10 12:17:30 +0530

    Update script.js

---
+++ Javascript_Other_Practice/Recipe Finder/script.js
@@ -0,0 +1,8 @@
+/* == API == */
+const API_BASE = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+const LOOKUP_BASE = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+
+/* == Variables == */
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn")
+
